b458e51d59fd60a0c60518ce3e3f95bdcccd329b
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-15 20:59:45 +0000

    created a functional navbar and inserted a temporary logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello, World!</h1>
+			<NavBar>
+			</NavBar>
 		</div>
 	);
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import logo from "../assets/globetrotters-logo.png";
+
+
+const NavBar = () => {
+    return (
+        <Navbar
+            bg="dark"
+            variant="dark"
+            expand="md"
+            fixed="top"
+        >
+            <Container>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="45" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+                        <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
+                        <Nav.Link>
+                            <Button><i className="fas fa-user-plus"></i>Sign up</Button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavBar;
