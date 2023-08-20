b0d1dfb336e949ed6918fd7dfd84ebcf57644954
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-20 12:51:06 +0000

    created Comment component so that users can display the comments they created

--- src/pages/journeys/JourneyPage.js
+++ src/pages/journeys/JourneyPage.js
@@ -10,18 +10,27 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Journey from "./Journey";
 
+import JourneyCommentCreateForm from "../../comments/journey_comments/JourneyCommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import JourneyComment from "../../comments/journey_comments/JourneyComment";
+
 function JourneyPage() {
     const { id } = useParams();
     const [journey, setJourney] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [journeyComments, setJourneyComments] = useState({ results: [] });
+
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: journey }] = await Promise.all([
+                const [{ data: journey }, {data: journeyComments}] = await Promise.all([
                     axiosReq.get(`/journeys/${id}`),
+                    axiosReq.get(`/journeys/comments/?journey=${id}`),
                 ])
                 setJourney({ results: [journey] });
-                console.log(journey);
+                setJourneyComments(journeyComments);
             } catch (err) {
                 console.log(err);
             }
@@ -34,7 +43,28 @@ function JourneyPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <Journey {...journey.results[0]} setJourney={setJourney} journeyPage />
-                <Container className={appStyles.Content}>Comments</Container>
+                <Container className={appStyles.Content}>
+                    {currentUser ? (
+                        <JourneyCommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            journey={id}
+                            setJourney={setJourney}
+                            setJourneyComments={setJourneyComments}
+                        />
+                    ) : journeyComments.results.length ? (
+                        "Comments"
+                    ) : null}
+                    {journeyComments.results.length ? (
+                        journeyComments.results.map((journeyComments) => (
+                            <JourneyComment key={journeyComments} {...journeyComments} />
+                        ))
+                    ) : currentUser ? (
+                        <p>Here we go</p>
+                    ) : (
+                        <p>No comments yet</p>
+                    )}
+                </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
                 <h3>Latest blogs</h3>
