4b8e71bf1e532d415f20007404d9abc185f0a4a4
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-08-20 20:38:04 +0000

    fixed errors and added handleUnfollow function

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,13 +1,15 @@
 import { createContext } from "react";
 import { useContext } from "react";
 import { useState } from "react";
-import { useCurrentUser } from "./CurrentUserContext";
 import { axiosRes } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { followHelper, unfollowHelper } from "../utils/utils";
 
 const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
 
-export const useProfileData = useContext(ProfileDataContext);
-export const useSetProfileData = useContext(setProfileDataContext);
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
     const [profileData, setProfileData] = useState({
@@ -31,14 +33,34 @@ export const ProfileDataProvider = ({ children }) => {
                 },
             }));
         } catch (err) {
-        console.log(err);
+            // console.log(err);
         }
-    }
+    };
+
+    const handleUnfollow = async (clickedProfile) => {
+        try {
+            await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+
+            setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: {
+                    results: prevState.pageProfile.results.map((profile) =>
+                        unfollowHelper(profile, clickedProfile)
+                    ),
+                },
+            }));
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+
     return (
-        <ProfileDataContext.Provider value={profileData} >
-            <setProfileDataContext.Provider value={setProfileData}>
+        <ProfileDataContext.Provider value={profileData}>
+            <SetProfileDataContext.Provider
+                value={{ setProfileData, handleFollow, handleUnfollow }}
+            >
                 {children}
-            </setProfileDataContext.Provider>
+            </SetProfileDataContext.Provider>
         </ProfileDataContext.Provider>
-    )
-}
+    );
+};
